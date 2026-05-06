a7d5782c4860fa0722056dc4fcd9ac48bce3b263
omegabenefits <admin@omegabenefits.net>
2026-05-06 13:16:50 -0600

    release 1.1

--- omega-cody.php
+++ omega-cody.php
@@ -13,7 +13,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'OMEGA_CODY_VERSION', '1.0.0' );
+define( 'OMEGA_CODY_VERSION', '1.1.0' );
 define( 'OMEGA_CODY_PLUGIN_FILE', __FILE__ );
 define( 'OMEGA_CODY_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 define( 'OMEGA_CODY_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
